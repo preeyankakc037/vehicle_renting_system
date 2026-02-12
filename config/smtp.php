24671f84e9cb34c1fb8fee94cdbf66eeecba727f
Priyanka Khatri <preeyankakc.07@gmail.com>
2026-02-12 16:48:49 +0545

    unnecessary file deleted

--- config/smtp.php
+++ config/smtp.php
@@ -5,8 +5,8 @@
 
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
-define('SMTP_USER', 'preeyankakc.07@gmail.com');
-define('SMTP_PASS', 'jcvnlgnpsyqyvwhq');
-define('SMTP_FROM', 'preeyankakc.07@gmail.com');
+define('SMTP_USER', 'yourrespectivegmail');
+define('SMTP_PASS', 'yourrespectivegmailpassword');
+define('SMTP_FROM', 'yourrespectivegmail');
 define('SMTP_FROM_NAME', 'Pathek');
 define('SMTP_SECURE', 'tls');
